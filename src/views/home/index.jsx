9dce16d543a74932512f1ce06289adfb13a38ef1
Luan Meira Quevedo <luan.quevedo@hotmail.com>
2024-03-25 21:13:55 -0300

    incluido a estilização do footer geral

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/header'
 import Search_bar from '../../components/search_Bar'
+import Footer from '../../components/footer'
 
 function Home() {
   return (
     <div>
       <Header/>
       <Search_bar/>
+      <Footer/>
     </div>
   )
 }
