d2229bbf6ed3ad67b407003d116b45dce3308c18
Luan Meira Quevedo <luan.quevedo@hotmail.com>
2024-03-26 15:45:38 -0300

    incluido arquivos dos livros e informações sobre os mesmos

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import Header from '../../components/header'
 import Search_bar from '../../components/search_Bar'
 import Footer from '../../components/footer'
+import Livros from '../../components/books'
 
 function Home() {
   return (
     <div>
       <Header/>
+      <div>
       <Search_bar/>
+      <Livros/>
+      </div>
+      
       <Footer/>
     </div>
   )
